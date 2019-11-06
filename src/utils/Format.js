6d604b03487de7354c6bfa0507e1dd4b2545f0dd
igorvieira87 <igor.vieira@loopix.com.br>
2019-11-06 15:11:53 -0200

    prototype

--- src/utils/Format.js
+++ src/utils/Format.js
@@ -1,7 +1,6 @@
 class Format {
     
     static getCamelCase(text){
-        debugger
         let div = document.createElement('div')
         div.innerHTML = `<div data-${text}="id"></div>`
         return Object.keys(div.firstChild.dataset)[0]
